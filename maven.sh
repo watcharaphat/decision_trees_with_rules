19fa775bc71103f63201839ca835d7176c64461e
Watcharaphat Manosatiankul <watcharaphat.3166@gmail.com>
2020-03-06 20:08:22 +0700

    correct dirname path is maven.sh

--- maven.sh
+++ maven.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run -it --rm -v `pwd`:/usr/src/decision_tree -v maven-m2:/root/.m2 -w /usr/src/decision_tree maven "$@"
+DIRNAME="$(cd $(dirname $0) && pwd)"
+docker run -it --rm -v ${DIRNAME}:/usr/src/decision_tree -v maven-m2:/root/.m2 -w /usr/src/decision_tree maven "$@"
